4a4befbe4da6c2626b3ebbc4aab67106b38f9d85
Koster2013 <Visually1@gmx.de>
2016-03-06 15:27:02 +0100

    first commit

---
+++ app/scripts/config.js
@@ -0,0 +1,178 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written stat for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/login");
+
+    $stateProvider
+
+        .state('dashboard', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "views/common/content.html",
+        })
+        .state('dashboard.main', {
+            url: "/main",
+            templateUrl: "views/dashboard/main/main.html",
+            data: { pageTitle: 'Example view' }
+        })
+        /* Dashboard*/
+
+        .state('dashboard.community', {
+            url: "/community",
+            templateUrl: "views/dashboard/community/community.html",
+            data: { pageTitle: 'community view' }
+        })
+        .state('dashboard.coupon', {
+            url: "/coupon",
+            templateUrl: "views/dashboard/coupon/coupon.html",
+            data: { pageTitle: 'Coupon view' }
+        })
+        .state('dashboard.infomaterial', {
+            url: "/infomaterial",
+            templateUrl: "views/dashboard/infomaterial/infomaterial.html",
+            data: { pageTitle: 'infomaterial view' }
+        })
+        .state('dashboard.list', {
+            url: "/list",
+            templateUrl: "views/dashboard/list/list.html",
+            data: { pageTitle: 'list view' }
+        })
+        .state('dashboard.promotion', {
+            url: "/promotion",
+            templateUrl: "views/dashboard/promotion/promotion.html",
+            data: { pageTitle: 'promotion view' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/moment/moment.min.js']
+                        },
+                        {
+                            name: 'ui.knob',
+                            files: ['js/plugins/jsKnob/jquery.knob.js','js/plugins/jsKnob/angular-knob.js']
+                        },
+                        {
+                            files: ['css/plugins/ionRangeSlider/ion.rangeSlider.css','css/plugins/ionRangeSlider/ion.rangeSlider.skinFlat.css','js/plugins/ionRangeSlider/ion.rangeSlider.min.js']
+                        },
+                        {
+                            insertBefore: '#loadBefore',
+                            name: 'localytics.directives',
+                            files: ['css/plugins/chosen/chosen.css','js/plugins/chosen/chosen.jquery.js','js/plugins/chosen/chosen.js']
+                        },
+                        {
+                            name: 'nouislider',
+                            files: ['css/plugins/nouslider/jquery.nouislider.css','js/plugins/nouslider/jquery.nouislider.min.js','js/plugins/nouslider/angular-nouislider.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css','js/plugins/datapicker/angular-datepicker.js']
+                        },
+                        {
+                            files: ['js/plugins/jasny/jasny-bootstrap.min.js']
+                        },
+                        {
+                            files: ['css/plugins/clockpicker/clockpicker.css', 'js/plugins/clockpicker/clockpicker.js']
+                        },
+                        {
+                            name: 'ui.switchery',
+                            files: ['css/plugins/switchery/switchery.css','js/plugins/switchery/switchery.js','js/plugins/switchery/ng-switchery.js']
+                        },
+                        {
+                            name: 'colorpicker.module',
+                            files: ['css/plugins/colorpicker/colorpicker.css','js/plugins/colorpicker/bootstrap-colorpicker-module.js']
+                        },
+                        {
+                            name: 'ngImgCrop',
+                            files: ['js/plugins/ngImgCrop/ng-img-crop.js','css/plugins/ngImgCrop/ng-img-crop.css']
+                        },
+                        {
+                            serie: true,
+                            files: ['js/plugins/daterangepicker/daterangepicker.js', 'css/plugins/daterangepicker/daterangepicker-bs3.css']
+                        },
+                        {
+                            name: 'daterangepicker',
+                            files: ['js/plugins/daterangepicker/angular-daterangepicker.js']
+                        },
+                        {
+                            files: ['css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css']
+                        },
+                        {
+                            name: 'ui.select',
+                            files: ['js/plugins/ui-select/select.min.js', 'css/plugins/ui-select/select.min.css']
+                        },
+                        {
+                            files: ['css/plugins/touchspin/jquery.bootstrap-touchspin.min.css', 'js/plugins/touchspin/jquery.bootstrap-touchspin.min.js']
+                        }
+
+                    ]);
+                }
+            }
+        })
+        .state('dashboard.tag', {
+            url: "/tag",
+            templateUrl: "views/dashboard/tag/tag.html",
+            data: { pageTitle: 'tag view' }
+        })
+        .state('dashboard.window', {
+            url: "/window",
+            templateUrl: "views/dashboard/window/window.html",
+            data: { pageTitle: 'Window view' }
+        })
+
+        .state('dashboard.profile', {
+            url: "/profile",
+            templateUrl: "views/dashboard/profile/profile.html",
+            controller: wizardCtrl,
+            data: { pageTitle: 'Wizard form' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['css/plugins/steps/jquery.steps.css']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('dashboard.profile.step_one', {
+            url: '/step_one',
+            templateUrl: 'views/dashboard/profile/wizard/step_one.html',
+            data: { pageTitle: 'Wizard form' }
+        })
+        .state('dashboard.profile.step_two', {
+            url: '/step_two',
+            templateUrl: 'views/dashboard/profile/wizard/step_two.html',
+            data: { pageTitle: 'Wizard form' }
+        })
+        .state('dashboard.profile.step_three', {
+            url: '/step_three',
+            templateUrl: 'views/dashboard/profile/wizard/step_three.html',
+            data: { pageTitle: 'Wizard form' }
+        })
+
+        /* Login Register */
+        .state('login', {
+            url: "/login",
+            templateUrl: "views/login/login.html",
+            data: { pageTitle: 'Login', specialClass: 'gray-bg'  }
+        })
+        .state('register', {
+            url: "/register",
+            templateUrl: "views/register/register.html",
+            data: { pageTitle: 'Register', specialClass: 'gray-bg' }
+        })
+}
+
+
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
---
+++ app/scripts/controllers.js
@@ -0,0 +1,36 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ */
+
+/**
+ * MainCtrl - controller
+ */
+function MainCtrl() {
+
+    this.userName = 'Example user';
+    this.helloText = 'Welcome in SeedProject';
+    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
+
+};
+
+/**
+ * wizardCtrl - Controller for wizard functions
+ * used in Wizard view
+ */
+function wizardCtrl($scope, $rootScope) {
+    // All data will be store in this object
+    $scope.formData = {};
+
+    // After process wizard
+    $scope.processForm = function() {
+        alert('Wizard completed');
+    };
+
+}
+
+
+angular
+    .module('inspinia')
+    .controller('MainCtrl', MainCtrl)
+    .controller('wizardCtrl', wizardCtrl)
